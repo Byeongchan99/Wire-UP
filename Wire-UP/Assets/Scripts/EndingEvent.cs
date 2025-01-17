eb7366a100413270abc0a30fcf1a5458276a9158
Lee Byoung Chan <harrrypoter@naver.com>
2025-01-17 19:52:25 +0900

    엔딩 이벤트 파티클 추가

--- Wire-UP/Assets/Scripts/EndingEvent.cs
+++ Wire-UP/Assets/Scripts/EndingEvent.cs
@@ -7,6 +7,7 @@ public class EndingEvent : MonoBehaviour
     public SphereCollider sphereCollider;
     // 파티클 시스템이 장착된 게임 오브젝트 리스트
     public List<GameObject> particleObjects = new List<GameObject>();
+    public List<ParticleSystem> particles = new List<ParticleSystem>();
 
     private void Start()
     {
@@ -19,6 +20,14 @@ public class EndingEvent : MonoBehaviour
                 particleObjects.Add(child.gameObject);
             }
         }
+
+        foreach (GameObject particleObject in particleObjects)
+        {
+            // 게임 오브젝트의 파티클 가져오기
+            ParticleSystem particleSystem = particleObject.GetComponentInChildren<ParticleSystem>();
+            // 파티클 리스트에 추가
+            particles.Add(particleSystem);
+        }
     }
 
     public void OnTriggerEnter(Collider other)
@@ -28,12 +37,10 @@ public class EndingEvent : MonoBehaviour
             Debug.Log("EndingEvent");
             // EndingEvent
             // 플레이어가 EndingEvent에 도달하면 파티클 시스템을 활성화
-            foreach (GameObject particleObject in particleObjects)
+            foreach (ParticleSystem particle in particles)
             {
-                // 게임 오브젝트의 파티클 가져오기
-                ParticleSystem particleSystem = particleObject.GetComponentInChildren<ParticleSystem>();
-                // 파티클 시스템 활성화
-                particleSystem.Play();
+                // 파티클 실행
+                particle.Play();
             }
         }
     }
